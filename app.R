20800146c6a2e54e572bf557862ff094cdb8deee
thebioengineer <ellishughes@live.com>
2019-03-05 23:03:29 -0800

    used processing code from raw.githack.com to come up with the regex/gsub to generate the cdn url.
    also adds meta tags dynamically

--- app.R
+++ app.R
@@ -1,22 +1,33 @@
 library(shiny)
 library(rvest)
-library(RCurl)
+library(jsonlite)
 
 validURL<-function(x){
-  isGH<-grepl("https[:]//(github[.]com)|(raw[.]githubusercontent[.]com)",x)
-  isValid<-RCurl::url.exists(x)
-  return(isGH && isValid)
+  if(!is.null(x)){
+    isGH<-grepl("^(https?):\\/\\/(raw)*(\\.)*github(?:usercontent)?\\.com",x)
+  }else{
+    isGH<-FALSE
+  }
+  return(isGH)
 }
 
-raw_html<-function(x){
-  if(!grepl("(https[:]//raw[.]githubusercontent[.]com)|([?]raw[=]true)",x)){
-    x<-paste0(x,"?raw=true")
-  }
-  iframe_html<-xml2::read_html(paste(readLines(x),collapse=""))
-  head<-as.character(html_children(html_node(iframe_html,"head")))
-  body<-as.character(html_children(html_node(iframe_html,"body")))
+githack_CDN<-function(url){
+  ghack<-gsub("^(https?):\\/\\/(raw)*(\\.)*github(?:usercontent)?\\.com\\/([^\\/]+\\/[^\\/]+\\/[^\\/]+|[0-9A-Za-z-]+\\/[0-9a-f]+\\/raw)\\/(.+)",
+              '\\1://raw.githack.com/\\4/\\5',url)
   
-  return(list(head=head,body=body))
+  apiToken<-gsub("^(\\w+:\\/\\/(raw).githack.com\\/([^\\/]+)\\/([^\\/]+))\\/([^\\/]+)\\/([^\\/]+)\\/(.*)",
+                 'https://api.github.com/repos/\\3/\\4/git/refs/heads/\\6',
+                 ghack)
+  
+  gh_sha<-jsonlite::fromJSON(apiToken)$object$sha
+  
+  newURL<-gsub("^(\\w+:\\/\\/(raw).githack.com\\/([^\\/]+)\\/([^\\/]+))\\/([^\\/]+)\\/([^\\/]+)\\/(.*)",
+               paste0('https://rawcdn.githack.com/\\3/\\4/',gh_sha,'/\\7'),
+               ghack)
+  
+  print(newURL)
+  
+  return(newURL)
 }
 
 shinyApp(
@@ -26,6 +37,7 @@ shinyApp(
     uiOutput("iframe",style="width: 100%;height: 100%;position: fixed;outline: none;"),
     title = "TwidgetServer",style='height: 100%;margin: 0;width:100%'))),
   server = function(input, output, session) {
+    
     #getting URL
     url <- reactiveVal()
     observe({
@@ -36,14 +48,20 @@ shinyApp(
         }else{
           query<-names(query)
         }
-        # if(validURL(query)){
+        if(validURL(query)){
           url(query)
-        # }else{
-        #   url("invalid_URL")
-        # }
+        }else{
+          url("invalid_URL")
+          if(any(is.null(query),query!="")){
+          updateTextInput(inputId = "ManualgithubURL",
+            placeholder = "<span style='color:red'>InvalidURL</span>",
+            session = session)
+          }
+        }
       }
     })
     
+    #update URL
     observeEvent(input$submitGHURL,{
       if(input$ManualgithubURL!=""){
         updateQueryString(paste0("?",input$ManualgithubURL),mode = c("replace", "push"))
@@ -51,21 +69,36 @@ shinyApp(
       }
     })
     
+    #render github url
     output$iframe<-renderUI({
       if( any(is.null(url()),
               length(url())==0,
               url()=="invalid_URL")){
-        tags$div(
+        tags$div(style="gravity:center;height:100%",
+                 tags$div(style="margin: 0 auto;text-align: center;",
+                          tags$div(
           tags$p("Enter github URL"),
           textInput("ManualgithubURL",label = "Github URL",value = ""),
           actionButton("submitGHURL",label = "Submit")
-        )
+        )))
       }else{
-        # url_html<-raw_html(url())
-        # tagList(tags$head(HTML(url_html$head)),HTML(url_html$body))
-        # # tags$link(rel='import',href=url())
-        # tags$iframe(src=paste0("http://htmlpreview.github.io/?",url()))
-        tags$iframe(src=url(),style='height: 100%;margin: 0;width:100%')
+        cdn_url<-githack_CDN(url())
+        
+        
+        
+        tagList(
+          tags$head(
+            tags$meta(name="twitter:card",content="player"),
+            tags$meta(name="twitter:title", content="Twidget: Served By Shiny"),
+            tags$meta(name="twitter:description", content="Click start the Twidget. Served by github and Shiny"),
+            tags$meta(name="twitter:player", content=cdn_url),
+            tags$meta(name="twitter:player:width", content="600"),
+            tags$meta(name="twitter:player:height", content="450"),
+            tags$meta(name="twitter:image", content="http://www.edu.uwo.ca/img/click_to_play.png")
+              
+          ),
+          tags$iframe(src=cdn_url,
+                    style='height: 100%;margin: 0;width:100%'))
         
       }
     })
